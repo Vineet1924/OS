a9e1318a52fe490a9bc2ce58379184cacb9e7c0b
Vineet Joshi <jvineet64@gmail.com>
2023-07-16 19:43:24 +0530

    update lab_1

--- LAB-1/Practical-1/Practical_1.c
+++ LAB-1/Practical-1/Practical_1.c
@@ -3,11 +3,12 @@
 
 int main(int argc, char *argv[]) {
     char *buff[2000];
-    int fd = open(argv[1], O_RDONLY, 0);
 
-    int len = read(fd, &buff, sizeof(buff));
-    write(1, buff, len);
+    for(int i = 1; i < argc; i++) {
+        int fd = open(argv[i], O_RDONLY, 0);
+        int len = read(fd, &buff, sizeof(buff));
+        write(1, buff, len);
+    }
 
-    close(fd);
     return 0;
 }
